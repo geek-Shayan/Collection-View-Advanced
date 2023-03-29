67c3a49b5e9ce1fb7337d0b85c879eda13e0748a
geek-Shayan <shayanulhaqsadi@gmail.com>
2023-03-29 09:38:08 +0600

    Initial Commit

---
+++ Collection View Advanced/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Collection View Advanced
+//
+//  Created by MD. SHAYANUL HAQ SADI on 29/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate {
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collection
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+ 
+        view.addSubview(collectionView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        collectionView.frame = view.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+    }
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = .orange
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+}
