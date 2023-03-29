42d159f04d98e94f5ab16fbbf1fb51353f3af4dc
geek-Shayan <shayanulhaqsadi@gmail.com>
2023-03-29 15:29:05 +0600

    more sections + reuse header

---
+++ Collection View Advanced/HeaderCollectionReusableView.swift
@@ -0,0 +1,42 @@
+//
+//  HeaderCollectionReusableView.swift
+//  Collection View Advanced
+//
+//  Created by MD. SHAYANUL HAQ SADI on 29/3/23.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+    
+    static let headerIdentifier = "header"
+    
+    private let header: UILabel = {
+        let label = UILabel()
+//        label.text = "Category"
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+//        label.backgroundColor = .darkGray
+        
+        return label
+    }()
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+//        header.backgroundColor = .brown
+        addSubview(header)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+//        header.frame = bounds
+        header.frame = bounds
+    }
+    
+    func setup(head: String) {
+        header.text = head
+    }
+}
--- Collection View Advanced/ViewController.swift
+++ Collection View Advanced/ViewController.swift
@@ -7,40 +7,100 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate {
+class ViewController: UIViewController {
+    
+    static let cellIdentifier = "cell"
+    static let categoryHeaderID = "categoryHeaderID"
+    static let categoryHeaderID2 = "categoryHeaderID2"
+//    static let headerIdentifier = "header"
     
     private let collectionView: UICollectionView = {
 //        let layout = UICollectionViewFlowLayout()
 //        layout.scrollDirection = .vertical
 
         let layout = UICollectionViewCompositionalLayout { sectionNumber, env in
-            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
-//            item.contentInsets.top = 16
-//            item.contentInsets.leading = 16
-            item.contentInsets.trailing = 2
-            item.contentInsets.bottom = 16
             
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200)), subitems: [item])
-//            (layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(300)), subitems: [item])
+            if (sectionNumber == 0) {
+                
+                let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+                item.contentInsets.trailing = 2
+                item.contentInsets.bottom = 16
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200)), subitems: [item])
+                //            (layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(300)), subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .paging
+                
+//                section.boundarySupplementaryItems = [ NSCollectionLayoutBoundarySupplementaryItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: categoryHeaderID2, alignment: .topLeading)]
+                
+                return section
+            }
+        
+            else if (sectionNumber == 1) {
+                let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25), heightDimension: .absolute(150)))
+                item.contentInsets.trailing = 16
+                item.contentInsets.bottom = 16
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(500)), subitems: [item])
+//                group.contentInsets.leading = 16
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                
+                section.boundarySupplementaryItems = [ NSCollectionLayoutBoundarySupplementaryItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: categoryHeaderID, alignment: .topLeading)]
+                
+                return section
+            }
             
-            let section = NSCollectionLayoutSection(group: group)
-            section.orthogonalScrollingBehavior = .paging
+            else if (sectionNumber == 2) {
+                let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+                item.contentInsets.trailing = 32
+                                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.80), heightDimension: .absolute(120)), subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .continuous
+                section.contentInsets.leading = 16
+
+                
+                section.boundarySupplementaryItems = [ NSCollectionLayoutBoundarySupplementaryItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: categoryHeaderID, alignment: .topLeading)]
+                
+                return section
+            }
             
-            return section
+            else {
+                let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .absolute(200)))
+                item.contentInsets.trailing = 16
+                item.contentInsets.bottom = 16
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(500)), subitems: [item])
+//                group.contentInsets.leading = 16
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                
+                section.boundarySupplementaryItems = [ NSCollectionLayoutBoundarySupplementaryItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: categoryHeaderID, alignment: .topLeading)]
+                
+                return section
+            }
         }
         
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: ViewController.cellIdentifier)
+//        collection.register(UICollectionReusableView.self, forSupplementaryViewOfKind: ViewController.categoryHeaderID, withReuseIdentifier: ViewController.headerIdentifier)
+        collection.register(HeaderCollectionReusableView.self, forSupplementaryViewOfKind: ViewController.categoryHeaderID, withReuseIdentifier: HeaderCollectionReusableView.headerIdentifier)
+//        collection.register(HeaderCollectionReusableView.self, forSupplementaryViewOfKind: ViewController.categoryHeaderID2, withReuseIdentifier: HeaderCollectionReusableView.headerIdentifier)
         return collection
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.delegate = self
+//        collectionView.delegate = self
         collectionView.dataSource = self
         
-        collectionView.backgroundColor = .gray
+        collectionView.backgroundColor = .darkGray
         navigationItem.title = "TITLE"
  
         view.addSubview(collectionView)
@@ -52,8 +112,29 @@ class ViewController: UIViewController, UICollectionViewDelegate {
 }
 
 extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        if section == 0 {
+            return 3
+        }
+        else if section == 1 {
+            return 8
+        }
+        else if section == 2 {
+            return 4
+        }
+        else {
+            return 5
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: HeaderCollectionReusableView.headerIdentifier, for: indexPath) as! HeaderCollectionReusableView
+        header.setup(head: "fskdjfdsjknfjk")
+        return header
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
@@ -61,14 +142,14 @@ extension ViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ViewController.cellIdentifier, for: indexPath)
         cell.backgroundColor = .orange
         return cell
     }
 }
 
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 50, height: 50)
-    }
-}
+//extension ViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 50, height: 50)
+//    }
+//}
